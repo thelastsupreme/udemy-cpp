2d3b88a738620ada4f495a57dc6eb1f07fa36384
thelastsupreme <39990949+thelastsupreme@users.noreply.github.com>
2020-05-19 22:45:06 +0530

    gg

--- Section12_Pointers and References/PassingPointers3/main.cpp
+++ Section12_Pointers and References/PassingPointers3/main.cpp
@@ -11,7 +11,7 @@ void display( const vector<string> *const  v) {
 //(*v).at(0) = "Funny";
     for (auto str: *v)
         cout << str << " ";
-    cout << endl;
+   // cout << endl;
   
  //   v = nullptr;
 }
