af9836c99a71c616a068803843a3ee21c36d3e71
thelastsupreme <39990949+thelastsupreme@users.noreply.github.com>
2020-06-02 11:08:41 +0530

    ..

--- Section19_IO Streams/ReadFile_1/main.cpp
+++ Section19_IO Streams/ReadFile_1/main.cpp
@@ -10,11 +10,12 @@ int main() {
     int num;
     double total;
     
-    in_file.open("../test.txt");
+    in_file.open("test.txt");
     if (!in_file) {
         std::cerr << "Problem opening file" << std::endl;
         return 1;
     }
+    
     in_file >> line >> num >> total;
     std::cout << line << std::endl;
     std::cout << num << std::endl;
